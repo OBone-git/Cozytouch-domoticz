151d49fe1b1b86773f4cba71be549a66faae046c
OBone-git <57813276+OBone-git@users.noreply.github.com>
2021-11-22 18:33:13 +0100

    Update cozytouch.py

--- cozytouch.py
+++ cozytouch.py
@@ -230,7 +230,7 @@ def domoticz_create_user_variable(nom_variable, valeur_variable):
     ''' création d'une variable utilisateur dans domoticz
     renvoie l'idx créé
     '''
-    # Requete de création de variable
+	# Requete de création de variable
     myurl=url_domoticz+'command&param=adduservariable&vname='+nom_variable+'&vtype=0&vvalue='+valeur_variable
     req=requests.get(myurl)
     if debug:
@@ -247,35 +247,38 @@ def domoticz_create_user_variable(nom_variable, valeur_variable):
             data=json.loads(req.text)
             if debug:
                     print(u'  '.join((u'GET-> ',myurl,' : ',str(req.status_code))).encode('utf-8'))
-
-            # Variable existante ou créée 
-            if data[u'status'] == ('Variable name already exists!') or ('OK'):
+            # Variable créée 
+            if data[u'status'] == ('OK'):
                 myurl=url_domoticz+'command&param=getuservariables'
                 req=requests.get(myurl)
                 if debug:
                     print(u'  '.join((u'GET-> ',myurl,' : ',str(req.status_code))).encode('utf-8'))
-                        
-        # Variable existante ou créée 
-        elif data[u'status'] == ('Variable name already exists!') or ('OK'):
+                # Réponse HTTP 200 OK
+                if req.status_code==200 :
+                    data=json.loads(req.text)
+                    # Sauvegarde idx
+                    for a in data[u'result']:
+                        if a[u'Name'] == nom_variable:
+                            idx = a[u'idx']
+                            return idx
+            else : print("!!!! Echec creation variable domoticz "+nom_variable)
+            
+        # Variable existante
+        if data[u'status'] == ('Variable name already exists!') or ('OK'):
             myurl=url_domoticz+'command&param=getuservariables'
             req=requests.get(myurl)
             if debug:
                 print(u'  '.join((u'GET-> ',myurl,' : ',str(req.status_code))).encode('utf-8'))
-
-                # Réponse HTTP 200 OK
-                if req.status_code==200 :
-                    data=json.loads(req.text)
-                    for a in data[u'result']:
-                        if a[u'Name'] == nom_variable:
-                            idx = a[u'idx']
-                    return idx
-                else:
-                    http_error(req.status_code,req.reason) # Appel fonction sur erreur HTTP
-            else:
-                print("!!!! Echec creation variable domoticz "+nom_variable)
+            # Réponse HTTP 200 OK
+            if req.status_code==200 :
+                data=json.loads(req.text)
+                # Sauvegarde idx
+                for a in data[u'result']:
+                    if a[u'Name'] == nom_variable:
+                        idx = a[u'idx']
+                        return idx
         else:
-            http_error(req.status_code,req.reason) # Appel fonction sur erreur HTTP
-
+            print("!!!! Echec creation variable domoticz "+nom_variable)
         return None
 
 def domoticz_rename_device(idx, nom):
